af3db055f1c9e7b792aa7d3146d8c4198f1f99aa
Ru Ruiyang <vivianru2006@126.com>
2025-08-07 13:55:37 +0200

    Add files via upload

---
+++ DateLabelFormatter.java
@@ -0,0 +1,26 @@
+package GroupProject2_Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class DateLabelFormatter {
+
+	String datePattern;
+	SimpleDateFormat dateFormatter;
+	
+	public Object stringToValue(String str) {
+		try {
+			return dateFormatter.parseObject(str);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return str;
+		}
+	}
+	
+	public String valueToString(Object obj) {
+		return dateFormatter.format(obj);
+	}
+	
+	
+}
