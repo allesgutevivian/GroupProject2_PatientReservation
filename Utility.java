e94e81d127ea9103454bb033f2e2dc9f1d59ab23
Hansada <134156016+CheongJea@users.noreply.github.com>
2025-08-07 15:00:18 +0200

    Update Utility.java

--- Utility.java
+++ Utility.java
@@ -3,10 +3,9 @@ package GroupProject2_Reservation;
 import javax.swing.JFrame;
 
 public class Utility {
-	public void logout(JFrame frame, Calendar calendar) {
-		
-		
-		
+	public void logout(JFrame frame, Calender calender) {
 		frame.dispose();
+		WelcomePage.login(calender);
 	}
 }
+
