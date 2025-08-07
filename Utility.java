7f61547383023fdd9349d0b213965996e7d3dbe9
Hansada <134156016+CheongJea@users.noreply.github.com>
2025-08-07 14:45:16 +0200

    Add files via upload

---
+++ Utility.java
@@ -0,0 +1,12 @@
+package GroupProject2_Reservation;
+
+import javax.swing.JFrame;
+
+public class Utility {
+	public void logout(JFrame frame, Calendar calendar) {
+		
+		
+		
+		frame.dispose();
+	}
+}
